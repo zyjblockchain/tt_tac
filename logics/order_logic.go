2f5f939432d4a1923306daf931e04d0aa5fcbf48
sandy <18382255942@163.com>
2020-05-28 13:15:46 +0800

    feat(): tac order duplicate checking

--- logics/order_logic.go
+++ logics/order_logic.go
@@ -37,6 +37,13 @@ func (ord *Order) CreateOrder() (uint, error) {
 		OrderType:     ord.OrderType,
 		State:         0,
 	}
+	// 查看数据库中是否存在相同的订单
+	o, err := order.GetOrder()
+	if err == nil && o != nil {
+		// 数据库中存在
+		log.Errorf("数据库中存在相同的跨链转账订单；FromAddr：%s, Amount: %s, OrderType: %d, State: %d", order.FromAddr, order.Amount, order.OrderType, order.State)
+		return 0, errors.New("数据库中存在相同的跨链转账订单，请修改转账金额或者等待上一个订单完成之后再重试")
+	}
 	// 保存到数据库
 	err = order.Create()
 	if err != nil {
